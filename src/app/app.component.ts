ca82215bd3e6aa2d0d5c63261ce22ce99149cd1e
Thisuri <36515881+ThisuriBandaranayake@users.noreply.github.com>
2018-06-10 09:23:00 +0530

    Just the template.Change it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: 5000, noPause: false, showIndicators: true } }
+  ]
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
-
+import { CarouselModule } from 'ngx-bootstrap';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot() ,
+    CarouselModule.forRoot(),
+    AngularFontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
